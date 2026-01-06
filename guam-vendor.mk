371e8e36f49213c3c705476087d19eb74abe6f5d
Michael Bestas <mkbestas@lineageos.org>
2026-01-06 16:14:55 +0200

    guam: Remove unused liblmutils-ns
    Change-Id: I59cdfe01baee2e72930436f294c0b990ddc87456

--- guam-vendor.mk
+++ guam-vendor.mk
@@ -211,7 +211,6 @@ PRODUCT_PACKAGES += \
     libgoodixhwfingerprint \
     libipebpsstriping \
     libjpege \
-    liblmutils-ns \
     libmctfengine_stub \
     libmmcamera_cac3 \
     libmmcamera_faceproc \
